cf8754086a4e1be2c4647f6ddd0f2e26f5fae808
Diego Pérez V <diego@flen.com.ec>
2023-09-08 11:36:07 -0500

    Footer links commented

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -7,11 +7,11 @@ export default function Footer () {
     <div className="relative flex justify-center bg-cover bg-center lg:h-80 pt-20" style={{ backgroundImage: 'url("/images/art-footer.png")' }}>
       <div className="flex flex-col items-center justify-center p-4 space-y-4 lg:space-y-0">
         <img src="/HCA-bn.svg" alt="Logo HCA" className="w-24 sm:w-40 h-auto lg:my-6" />
-        <div className="flex flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-8 text-hcadarkblue">
+        {/* <div className="flex flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-8 text-hcadarkblue">
           <a href="#" className=" hover:underline">Política de privacidad</a>
           <a href="#" className=" hover:underline">Política de cookies</a>
           <a href="#" className=" hover:underline">¿Tienes dudas o sugerencias? ¡Escríbenos!</a>
-        </div>
+        </div> */}
       </div>
     </div>
     <Image
