91c19a9ecdb204e12911d84739788f3b61f39e1f
Diego Pérez V <diego@flen.com.ec>
2023-09-08 16:39:45 -0500

    repited text eliminated

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,6 @@ export default function Home () {
       <div className="flex flex-col justify-center items-center text-center my-20 lg:my-40 px-8">
         <h1 className='text-4xl text-hcadarkblue font-bold'>¡Espéranos muy pronto!</h1>
         <h3 className='text-2xl text-black mt-16'>Estamos alistándonos para transformar la vibra de Quevedo y Riobamba.</h3>
-        <h3 className='text-2xl text-black mt-16'>Estamos alistándonos para transformar la vibra de Quevedo y Riobamba.</h3>
       </div>
     </main>
   )
