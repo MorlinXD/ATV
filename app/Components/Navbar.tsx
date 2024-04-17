3b62942b5a21ffdf0301d1a8fedf9be77bd25cb1
Diego Pérez V <diego@flen.com.ec>
2024-04-16 21:54:25 -0500

    New fixes completed

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,68 +1,9 @@
-// import { usePathname } from 'next/navigation'
-// import Link from 'next/link'
-
-// const navItems = [
-//   {
-//     path: '/alza-tu-voz',
-//     name: '¿Qué es ALZA TU VOZ?'
-//   },
-//   {
-//     path: '/como-lo-hacemos',
-//     name: '¿Cómo lo hacemos?'
-//   },
-//   {
-//     path: '/riobamba',
-//     name: 'Riobamba'
-//   },
-//   {
-//     path: '/quevedo',
-//     name: 'Quevedo'
-//   },
-//   {
-//     path: '/lo-que-se-viene',
-//     name: 'Lo que se viene'
-//   }
-//   // {
-//   //   path: '/convocatorias',
-//   //   name: 'Convocatorias'
-//   // }
-// ]
-
-// export default function Navbar () {
-//   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-//   const pathname = usePathname() || '/'
-//   return (
-//   <div className='font-bold'>
-//     <nav className="flex flex-col md:flex-row w-full z-[100] rounded-full p-2 bg-hcanewblue">
-//       {navItems.map((item) => {
-//         const isActive = item.path === pathname
-
-//         return (
-//           <Link
-//             key={item.path}
-//             className={`px-4 py-2 rounded-full text-sm lg:text-base relative no-underline duration-300 ease-in text-white ${
-//               isActive ? 'bg-hcaneworange' : 'bg-hcanewblue'
-//             }`}
-//             href={item.path}
-//           >
-//             <span>{item.name}</span>
-//           </Link>
-//         )
-//       })}
-//     </nav>
-//   </div>
-//   )
-// }
 'use client'
 import React from 'react'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 
 const navItems = [
-  // {
-  //   path: '/alza-tu-voz',
-  //   name: '¿Qué es ALZA TU VOZ?'
-  // },
   {
     name: '¿Qué es ALZA TU VOZ?',
     submenu: [
@@ -71,10 +12,6 @@ const navItems = [
       { path: '/alza-tu-voz#como-nos-organizamos', name: '¿Cómo nos organizamos?' }
     ]
   },
-  // {
-  //   path: '/como-lo-hacemos',
-  //   name: '¿Cómo lo hacemos?'
-  // },
   {
     name: '¿Cómo lo hacemos?',
     submenu: [
@@ -97,13 +34,13 @@ export default function Navbar () {
   const pathname = usePathname() || '/'
 
   return (
-    <div className='font-bold'>
-      <nav className="flex flex-col md:flex-row w-full z-10 rounded-full p-2 md:bg-hcanewblue">
-        {navItems.map((item) => {
-          const isActive = item.path === pathname
-          const isSubMenu = item.submenu !== undefined
+  <div className='font-bold'>
+    <nav className="flex flex-col md:flex-row w-full z-10 rounded-full p-2 md:bg-hcanewblue">
+      {navItems.map((item) => {
+        const isActive = item.path === pathname
+        const isSubMenu = item.submenu !== undefined
 
-          return (
+        return (
             <div key={item.name} className="relative">
               {isSubMenu
                 ? (
@@ -134,8 +71,8 @@ export default function Navbar () {
                 </Link>
                   )}
             </div>
-          )
-        })}
+        )
+      })}
       </nav>
     </div>
   )
