88b51ccf1af502d7fdc8ef5a9fe6f8dabaac940f
Diego Pérez <diego@datalat.org>
2024-07-18 10:47:33 -0500

    Nwe footer merge

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -4,13 +4,24 @@ export default function Footer () {
   return (
     <footer>
       <div className='container mx-auto'>
-        <Image
+        {/* <Image
           src="/footer-img.png"
           alt="Logos de socios del programa Healthy Cities Ecuador"
           width={1920}
           height={379}
           priority
-        />
+        /> */}
+        <div>
+          <p>sdfsdfsd</p>
+          <img src="/images/pilar-agentes.svg" alt="Logo" className="h-24 mb-10" />
+        </div>
+        <div className='flex'>
+          <p>sdfsdfsd</p>
+          <img src="/images/pilar-agentes.svg" alt="Logo" className="h-24 mb-10" />
+          <img src="/images/pilar-agentes.svg" alt="Logo" className="h-24 mb-10" />
+          <img src="/images/pilar-agentes.svg" alt="Logo" className="h-24 mb-10" />
+          <img src="/images/pilar-agentes.svg" alt="Logo" className="h-24 mb-10" />
+        </div>
       </div>
     </footer>
   )
