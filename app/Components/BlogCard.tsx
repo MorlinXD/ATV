e45dbf6e04002833bf09f8c9894db91add6247a2
Diego Pérez V <diego@flen.com.ec>
2024-09-06 15:19:39 -0500

    Link in all blog card

--- app/Components/BlogCard.tsx
+++ app/Components/BlogCard.tsx
@@ -10,18 +10,18 @@ interface BlogCardProps {
 
 const BlogCard: React.FC<BlogCardProps> = ({ image, name, date, link }) => {
   return (
-    <figure className="flex flex-col bg-white p-4 shadow-lg rounded-xl w-96 min-w-[400px]">
+    <Link href={link}>
+      <figure className="flex flex-col bg-white p-4 shadow-lg rounded-xl w-96 min-w-[400px] cursor-pointer">
       <img className="py-4" src={image} alt="Blog image" />
       <h3 className='font-bold text-hcanewblue'>{name}</h3>
       <div className='flex justify-between'>
         <p className="mt-2">
           {date}
         </p>
-        <Link href={link}>
-          <img className="w-10 h-10" src='/images/row-icon.svg' alt="Logo" />
-        </Link>
+        <img className="w-10 h-10" src='/images/row-icon.svg' alt="Logo" />
       </div>
-    </figure>
+      </figure>
+    </Link>
   )
 }
 
