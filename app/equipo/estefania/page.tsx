491b5b1c54abafce89dd0d116c0116fcc5c37213
Diego Pérez V <diego@flen.com.ec>
2024-09-06 09:54:48 -0500

    Team profiles added

---
+++ app/equipo/estefania/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function EstefaniaTeam () {
+  return (
+    <div className="container mx-auto flex justify-center my-20 px-6">
+      <Image
+        src='/images/equipo/Belen.png'
+        alt='Estefania Checa'
+        width={600}
+        height={900}
+        className="rounded-3xl"
+      />
+    </div>
+  )
+}
