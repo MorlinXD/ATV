2457d2e1f45aa0823871b953f5276373a1de2348
Diego Pérez V <diego@flen.com.ec>
2024-11-24 13:08:55 -0500

    Responsive menu links fixed

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -36,38 +36,44 @@ const navItems = [
 ]
 
 export default function Navbar () {
-  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   const pathname = usePathname() || '/'
 
   return (
-  <div className='font-bold'>
-    <nav className="flex flex-col md:flex-row w-full z-10 rounded-full p-2 md:bg-hcanewblue">
-      {navItems.map((item) => {
-        const isActive = item.path === pathname
-        const isSubMenu = item.submenu !== undefined
+    <div className='font-bold'>
+      <nav className="flex flex-col md:flex-row w-full z-10 rounded-full p-2 md:bg-hcanewblue">
+        {navItems.map((item) => {
+          const isActive = item.path === pathname
+          const isSubMenu = item.submenu !== undefined
 
-        return (
+          return (
             <div key={item.name} className="relative">
-              {isSubMenu
-                ? (
+              {isSubMenu ? (
                 <div className="group">
                   <p className={`px-4 py-2 md:rounded-full text-sm lg:text-base no-underline duration-300 ease-in text-white ${
                     isActive ? 'bg-hcaneworange' : 'bg-hcanewblue'
                   }`}>
                     {item.name}
                   </p>
-                  <div className="absolute left-0 opacity-0 group-hover:opacity-100 mt-2 py-2 w-48 bg-hcanewblue rounded-md shadow-xl z-20 transition-opacity duration-300">
+                  <div className={`
+                    md:absolute md:left-0 
+                    md:opacity-0 md:group-hover:opacity-100 
+                    md:mt-2 
+                    w-full md:w-48 
+                    bg-hcanewblue 
+                    md:rounded-md md:shadow-xl 
+                    z-20 
+                    md:transition-opacity md:duration-300
+                  `}>
                     {item.submenu.map(subItem => (
                       <Link key={subItem.path} href={subItem.path}>
-                        <p className="block px-4 py-2 text-sm text-white hover:bg-hcaneworange">
+                        <p className="block pl-8 md:pl-4 py-2 text-sm text-white hover:bg-hcaneworange">
                           {subItem.name}
                         </p>
                       </Link>
                     ))}
                   </div>
                 </div>
-                  )
-                : (
+              ) : (
                 <Link href={item.path}>
                   <p className={`px-4 py-2 md:rounded-full text-sm lg:text-base no-underline duration-300 ease-in text-white ${
                     isActive ? 'bg-hcaneworange' : 'bg-hcanewblue'
@@ -75,10 +81,10 @@ export default function Navbar () {
                     {item.name}
                   </p>
                 </Link>
-                  )}
+              )}
             </div>
-        )
-      })}
+          )
+        })}
       </nav>
     </div>
   )
