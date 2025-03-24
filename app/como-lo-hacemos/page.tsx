2ef144e56c1d4e18a37d872ce3719e08d6f39c27
Diego Pérez V <diego@flen.com.ec>
2025-03-24 17:53:00 -0500

    Refactor content layout and update survey results section with new title and download button

--- app/como-lo-hacemos/page.tsx
+++ app/como-lo-hacemos/page.tsx
@@ -299,63 +299,22 @@ export default function Home () {
               </div>
           </div>
         </div>
-
-      {/* <div className='mb-20'>
-        <div className='container mx-auto flex flex-col justify-center items-center xl:flex-row gap-10 md:gap-20'>
-          <div className="md:w-1/3 mx-4 relative">
-            <Image
-                src="/images/clh-img-1.png"
-                alt="Mapa con lupa"
-                width={645}
-                height={645}
-              />
-          </div>
-          <div className=' rounded-[4rem] mx-4'>
-            <div className='p-2 md:p-8 flex flex-col md:mb-4 gap-4'>
-              <div className="flex items-center justify-between bg-white rounded-xl 2xl:rounded-full px-10 py-2">
+        <div className='flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-4 p-8 mt-5'>
+          <div className="flex flex-col justify-center items-center w-full mx-20 md:w-1/2 h-auto">
+            <div className="flex flex-col items-center justify-between bg-white rounded-xl p-10">
                 <div className='flex flex-col'>
-                  <p className='text-2xl font-bold text-hcanewblue mb-2'>Guía para levantar la voz. Caja de herramientas participativas</p>
-                  <span className="text-lg">Herramientas para que adolescentes sean líderes en hacer que las cosas mejoren en sus comunidades. </span>
+                  <p className='text-2xl font-bold text-hcanewblue mb-2'>Incidiendo en el espacio público a través de los datos</p>
+                  <span className="text-lg">Resultado de los datos levantados en el espacio público de Quevedo y Riobamba, antes y durante los “Festivales de Intervenciones Urbanas Alza Tu Voz”.</span>
                 </div>
-                <DownloadButton
-                  link='https://drive.google.com/file/d/1th0SJu6gu4oKgPM7WMuiph9S7BpPF8Ee/view'
+                <NewButtonTwo
+                  color='bg-hcaneworange'
+                  label='Descargar resultados'
+                  link='https://drive.google.com/file/d/1GjFGfUBmwU7zHlJgXSkOqFIdxaFYRRb3/view?usp=sharing'
                   target='_blank'
                 />
               </div>
-              <div className="flex items-center justify-between bg-white rounded-xl 2xl:rounded-full px-10 py-2">
-                <div className='flex flex-col'>
-                  <p className='text-2xl font-bold text-hcanewblue mb-2'>Caja de herramientas. Una guía para diseñar juntos soluciones a problemas sociales con la comunidad</p>
-                  <span className="text-lg">Metodologías para promover la participación, abordar las necesidades de adolescentes, y co-diseñar la Teoría del Cambio.</span>
-                </div>
-                <DownloadButton
-                  link='https://drive.google.com/file/d/1qO0TEfboNzN15W4dBY8e4irZysE4ymFM/view'
-                  target='_blank'
-                />
-              </div>
-              <div className="flex items-center justify-between bg-white rounded-xl 2xl:rounded-full px-10 py-2">
-                <div className='flex flex-col'>
-                  <p className='text-2xl font-bold text-hcanewblue mb-2'>Resultados de la encuesta: "Mi ciudad y yo"</p>
-                  <span className="text-lg">Encuesta realizada en Quevedo y Riobamba para comprender la relación de adolescentes y jóvenes con su entorno, e identificar sus necesidades.</span>
-                </div>
-                <DownloadButton
-                  link='https://drive.google.com/file/d/19cynme6_Zo8SySp3Ose20GiIOIB3qn8e/view'
-                  target='_blank'
-                />
-              </div>
-              <div className="flex items-center justify-between bg-white rounded-xl 2xl:rounded-full px-10 py-2">
-                <div className='flex flex-col'>
-                  <p className='text-2xl font-bold text-hcanewblue mb-2'>Resultados de la encuesta: "Imaginando mi futuro"</p>
-                  <span className="text-lg">Encuesta realizada en Quevedo y Riobamba para comprender los sueños y la percepción de adolescentes y jóvenes sobre acciones positivas para construir su futuro.</span>
-                </div>
-                <DownloadButton
-                  link='https://drive.google.com/file/d/1IsIxLvpdS4vAjOtjWaqvN3G69XDUIkVq/view'
-                  target='_blank'
-                />
-              </div>
-            </div>
           </div>
         </div>
-      </div> */}
     </section>
     </main>
   )
