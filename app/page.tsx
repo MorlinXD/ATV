217e0ec244ede9482eb45ea4d102b0c7b80b17b9
MorlinXD <jeanmorales102002@gmail.com>
2025-10-27 02:44:42 -0500

    Corregido logo ATV

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <div className="flex flex-col items-center md:items-start text-center md:text-left">
           {/* Logo */}
           <Image
-            src="/images/index/new_menu/lgATV.png"
+            src="/images/index/new_menu/logATV.png"
             alt="Logo Alza Tu Voz"
             width={420}
             height={210}
