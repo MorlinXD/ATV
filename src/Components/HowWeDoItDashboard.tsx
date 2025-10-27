135e92456f39f4605b3c813d448f30bc5327f9e9
MorlinXD <jeanmorales102002@gmail.com>
2025-10-27 03:15:35 -0500

    ajust cards

--- src/Components/HowWeDoItDashboard.tsx
+++ src/Components/HowWeDoItDashboard.tsx
@@ -16,21 +16,21 @@ export default function HowWeDoItDashboard() {
         <p className="text-center"> Conócelos, navegando en el visualizador. </p>
         <p className="text-center mb-10"> ¡Te invitamos a descubrir lo que estamos pensando! </p>
 
-        <section id="tarjetas" className="text-center mb-10 flex flex-wrap justify-center gap-6">
+        <section id="tarjetas" className="text-center mb-10 flex flex-row justify-center items-center gap-6">
           <img
             src="/images/resultados/img1.png"
             alt="Ilustración de caminatas dateras"
-            className="w-48 sm:w-56 md:w-64 h-auto hover:scale-105 transition-transform"
+            className="w-56 sm:w-64 md:w-72 h-auto hover:scale-105 transition-transform"
           />
           <img
             src="/images/resultados/Group 201.png"
             alt="Ilustración de Mi ciudad y yo"
-            className="w-48 sm:w-56 md:w-64 h-auto hover:scale-105 transition-transform"
+            className="w-56 sm:w-64 md:w-72 h-auto hover:scale-105 transition-transform"
           />
           <img
             src="/images/resultados/Group 202.png"
             alt="Ilustración de  Imaginando mi futuro"
-            className="w-48 sm:w-56 md:w-64 h-auto hover:scale-105 transition-transform"
+            className="w-56 sm:w-64 md:w-72 h-auto hover:scale-105 transition-transform"
           />
         </section>
         <section id="Grafico-PWBI">
