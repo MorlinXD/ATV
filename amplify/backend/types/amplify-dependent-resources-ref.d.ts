26118a375d503043f64bb512be45cdd30d3d6639
MorlinXD <jeanmorales102002@gmail.com>
2025-12-07 22:17:04 -0500

    Apmlify auth para usuarios

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "labxxi20257f80cc9e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
